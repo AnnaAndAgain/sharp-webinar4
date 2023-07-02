b55d1d35314c4242db0f2a2b8ce6b2b6f24bbce7
Anna <a.s.markovskaya@gmail.com>
2023-07-02 11:56:50 +0300

    task 1

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Принимаем на вход число А, выдаем сумму чмисел от 1 до А
+*/
+
+
+int ReadInt(string text) 
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumFromOne(int endNumber) 
+{
+    int sum = 0;
+    if (endNumber > 0)
+    {
+        for (int i = 1; i <= endNumber; i++) 
+        {
+            sum = sum + i;
+        }
+    }
+    else System.Console.WriteLine("Число слишком маленькое");
+    return sum;
+};
+
+int A = ReadInt("Введите число, до которого будем считать  сумму");
+int result = SumFromOne(A);
+System.Console.WriteLine(result);
