2c46a85dbb5fd048e70b1f319820bd38a6a248c7
Anna <a.s.markovskaya@gmail.com>
2023-07-02 13:16:06 +0300

    + task 2

---
+++ task2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Число на вход, число цифр на выходе
+*/
+
+int ReadInt(string text) 
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int CountDigits(int number)
+{
+    number = Math.Abs(number);
+    int count = 0;
+    int i = 10;
+    while (number > 0)
+    {
+        number = number - number%i;
+        count = count + 1;
+        i=i*10;
+    }
+    return count;
+}
+
+int N = ReadInt("Введите число:");
+System.Console.WriteLine(CountDigits(N));
+
